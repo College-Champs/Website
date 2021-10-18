70025148ee8958d2be3eac7b2a78094a566177dd
stranothus <stranothus@gmail.com>
2021-10-17 20:37:25 -0500

    Middlewares and Express Skeleton

--- index.js
+++ index.js
@@ -2,7 +2,18 @@
 exports.__esModule = true;
 var fs = require("fs");
 var express = require("express");
+var dotenv = require("dotenv");
+var mongodb_1 = require("mongodb");
+dotenv.config();
+var DB = new Promise(function (resolve, reject) {
+    mongodb_1.MongoClient.connect("mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@cluster0.jujqg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", function (err, db) {
+        if (err)
+            console.error(err);
+        resolve(db);
+    });
+});
 var app = express();
+var pages = express.Router();
 app.get("/", function (req, res) {
     res.send("Hello, World!");
 });
--- index.ts
+++ index.ts
@@ -1,9 +1,42 @@
-import { Application } from "express";
+import { Application, Router } from "express";
 
-const fs:object = require("fs");
-const express:Function = require("express");
+const fs = require("fs");
+const express = require("express");
+const multer = require("multer");
+const dotenv = require("dotenv");
+import { MongoClient } from "mongodb";
+import { StorageEngine } from "multer";
+
+dotenv.config();
+
+var DB:Promise<MongoClient> = new Promise((resolve, reject):void => {
+    MongoClient.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jujqg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+        (err:Error, db:MongoClient):void => {
+            if(err) console.error(err);
+
+            resolve(db);
+        }
+    );
+});
 
 const app:Application = express();
+
+const pages:Router = express.Router();
+const api:Router = express.Router();
+const storage:StorageEngine = multer.diskStorage({
+    destination: (req:Express.Request, file:Express.Multer.File, cb:(error: Error | null, destination: string) => void) => {
+      	cb(null, __dirname + "/public/uploads");
+    },
+    filename: (eq:Express.Request, file:Express.Multer.File, cb:(error: Error | null, destination: string) => void) => {
+      	cb(null, file.originalname);
+    }
+});
+const upload = multer({ storage: storage });
+
+app.use(express.json());
+app.use(upload.array("file"));
+app.use("/pages", pages);
+app.use("/api", api);
 
 app.get("/", (req, res):void => {
     res.send("Hello, World!");
